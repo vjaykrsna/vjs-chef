366c0532c7361095aae295b8d7e990072a378336
vjaykrsna <vijay74684@gmail.com>
2025-06-14 14:45:12 +0530

    trying to fix broken logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header () {
     return (
         <header>
-            <img src="/chef-claude-icon.png" alt="logo" />
+            <img src="logo.png" alt="logo" />
             <h1>VJ's Chef</h1>
         </header>
     )
